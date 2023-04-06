d7e808784e0f7c03ce3da6827b14d15dd980c168
dusann14 <dusan.stoimenovicvr@gmail.com>
2023-04-06 19:29:46 +0200

    Dodata nova implemntacija metode

--- Magacin/src/interfejsimpl/SpecifikacijaInterfejsImpl.java
+++ Magacin/src/interfejsimpl/SpecifikacijaInterfejsImpl.java
@@ -3,12 +3,19 @@ package interfejsimpl;
 import domen.Artikal;
 import intefejs.SpecifikacijaInterfejs;
 
+import java.util.LinkedList;
+import java.util.List;
 public class SpecifikacijaInterfejsImpl implements SpecifikacijaInterfejs{
 
+	private List<Artikal> artikli = new LinkedList<Artikal>();
+	
 	@Override
 	public void ubaciArtikal(Artikal a) {
 		// TODO Auto-generated method stub
+		if(a == null)
+			return;
 		
+		artikli.add(a);
 	}
 
 	@Override

c4aecfb890bd19711e3cfcc8efd24991749694cd
dusann14 <dusan.stoimenovicvr@gmail.com>
2023-04-06 18:57:22 +0200

    Dodate klase artikal

---
+++ Magacin/src/domen/Artikal.java
@@ -0,0 +1,36 @@
+package domen;
+
+public class Artikal {
+	
+	private String naziv,opis;
+	private int sifra, kolicina;
+	
+	
+	
+	public String getNaziv() {
+		return naziv;
+	}
+	public void setNaziv(String naziv) {
+		this.naziv = naziv;
+	}
+	public String getOpis() {
+		return opis;
+	}
+	public void setOpis(String opis) {
+		this.opis = opis;
+	}
+	public int getSifra() {
+		return sifra;
+	}
+	public void setSifra(int sifra) {
+		this.sifra = sifra;
+	}
+	public int getKolicina() {
+		return kolicina;
+	}
+	public void setKolicina(int kolicina) {
+		this.kolicina = kolicina;
+	}
+	
+	
+}

52d2ed38906bd994ef0c8814a73f3d2d545f2940
dusann14 <dusan.stoimenovicvr@gmail.com>
2023-04-06 19:20:13 +0200

    Dodata klasa za implementaciju

---
+++ Magacin/src/interfejsimpl/SpecifikacijaInterfejsImpl.java
@@ -0,0 +1,26 @@
+package interfejsimpl;
+
+import domen.Artikal;
+import intefejs.SpecifikacijaInterfejs;
+
+public class SpecifikacijaInterfejsImpl implements SpecifikacijaInterfejs{
+
+	@Override
+	public void ubaciArtikal(Artikal a) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void izbaciArtikal(Artikal a) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Artikal pronadjiArtikal(int sifra) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
